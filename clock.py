c3dd98903832d16737987977de9216149d931983
unknown <saniaprasad2910@gmail.com>
2026-09-13 10:17:26 +0530

    some more change

--- clock.py
+++ clock.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from datetime import datetime
+import winsound
 
 root = tk.Tk()
 
@@ -7,7 +8,7 @@ root.title("Useless Alarm Clock")
 root.geometry("600x400")
 root.configure(bg="#111111")
 
-
+snooze_count = 0
 # ---------------- PAGE 1 : CLOCK ----------------
 
 clock_page = tk.Frame(root, bg="#111111")
@@ -95,7 +96,22 @@ seconds_entry = tk.Entry(
 )
 seconds_entry.pack(pady=15)
 
+def alarm_ringing():
+    alarm_page.pack_forget()
+    clock_page.pack_forget()
 
+    ringing_page.pack(fill="both", expand=True)
+
+    if snooze_count > 5:
+        ringing_title.config(
+            text="⏰ I'M DONE"
+        )
+    else:
+        ringing_title.config(
+            text="⏰ WAKE UP!!!"
+        )
+
+    winsound.Beep(1000, 1000)
 def set_alarm():
     try:
         seconds = int(seconds_entry.get())
@@ -109,6 +125,8 @@ def set_alarm():
         confirmation.config(
             text=f"Alarm set for {seconds} seconds!"
         )
+
+        root.after(seconds * 1000, alarm_ringing)
 
     except ValueError:
         confirmation.config(
@@ -134,12 +152,94 @@ confirmation = tk.Label(
 )
 confirmation.pack(pady=10)
 
+# ---------------- PAGE 3 : ALARM RINGING ----------------
+
+ringing_page = tk.Frame(root, bg="#111111")
+
+ringing_title = tk.Label(
+    ringing_page,
+    text="⏰ WAKE UP!!!",
+    font=("Arial", 40, "bold"),
+    bg="#111111",
+    fg="white"
+)
+ringing_title.pack(pady=(80, 20))
+
+
+ringing_message = tk.Label(
+    ringing_page,
+    text="YOUR ALARM IS RINGING",
+    font=("Arial", 18),
+    bg="#111111",
+    fg="white"
+)
+ringing_message.pack(pady=10)
+
+
+# This is the ACTUAL snooze function
+def actual_snooze():
+    global snooze_count
+
+    snooze_count += 1
+
+    winsound.PlaySound(None, winsound.SND_PURGE)
+
+    if snooze_count == 1:
+        message = "SIKE YOU SNOOZED HEHE"
+
+    elif snooze_count == 2:
+        message = "Okay I'll dismiss next time"
+
+    elif snooze_count == 3:
+        message = "Next time for sure"
+
+    elif snooze_count == 4:
+        message = "Get Up Plis"
+
+    else:
+        message = "ACHIEVEMENT UNLOCKED!! \n SUCCESSFULLY DISAPPOINTED THE ENTIRE BLOODLINE"
+
+    ringing_message.config(text=message)
+
+    root.after(5000, alarm_ringing)
+
+
+# This is the ACTUAL dismiss function
+def actual_dismiss():
+    winsound.PlaySound(None, winsound.SND_PURGE)
+
+    ringing_message.config(
+        text="ALARM DEFEATED \n YOU WIN!!"
+    )
+
+    root.after(1500, go_to_clock)
+
+
+# SWAPPED BUTTONS
+dismiss_button = tk.Button(
+    ringing_page,
+    text="DISMISS",
+    font=("Arial", 16, "bold"),
+    command=actual_snooze
+)
+dismiss_button.pack(pady=15)
+
+
+snooze_button = tk.Button(
+    ringing_page,
+    text="SNOOZE",
+    font=("Arial", 16, "bold"),
+    command=actual_dismiss
+)
+snooze_button.pack(pady=15)
 
 def back_to_clock():
     alarm_page.pack_forget()
     clock_page.pack(fill="both", expand=True)
 
-
+def go_to_clock():
+    ringing_page.pack_forget()
+    clock_page.pack(fill="both", expand=True)
 back_button = tk.Button(
     alarm_page,
     text="← BACK",
